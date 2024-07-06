eb2e7a3bdba42d4872edd3974abe1afa1f2f13cb
corni <gmigan.a@gmail.com>
2024-07-06 22:02:34 +0200

    fix: exams launch a count down of 4 hours

--- frontend/TimeDisplay.py
+++ frontend/TimeDisplay.py
@@ -4,7 +4,7 @@ from time import monotonic
 
 class TimeDisplay(Static):
 
-   start_time = reactive(monotonic)
+   start_time = reactive(monotonic() + 60 * 60 * 4)
    time = reactive(0.0)
 
    def on_mount(self) -> None:
@@ -12,14 +12,11 @@ class TimeDisplay(Static):
       self.set_interval(1 / 60, self.update_time)
 
    def update_time(self) -> None:
-      self.time = monotonic() - self.start_time
+      self.time = self.start_time - monotonic()
 
    def watch_time(self, time: float) -> None:
       minutes, seconds = divmod(time, 60)
       hours, minutes = divmod(minutes, 60)
-      hours = int(3 - hours)
-      minutes = 59 if minutes == 0 else 59 - minutes
-      seconds = int(60 - seconds)
-      self.update(f"{int(hours)}:{minutes:02.0f}:{seconds}")
+      self.update(f"{int(hours):02.0f}:{minutes:02.0f}:{seconds:02.0f}")
 
 
