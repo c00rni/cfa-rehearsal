b8d46e6659ee4c82d0f612265d3a245b2d19c98b
corni <gmigan.a@gmail.com>
2024-07-07 17:05:02 +0200

    fix: Once the next button is click a new question is displayed on the screen

--- frontend/App.py
+++ frontend/App.py
@@ -1,20 +1,41 @@
 from textual.app import App
 from textual.widgets import Header, Footer
+from textual.containers import ScrollableContainer
 from MockExam import MockExam
 from MainMenu import MainMenu
 
 class CfaPrepApp(App):
    BINDINGS = [
       ("q", "quit", "Quit the app"),
+      ("s", "launch_exam", "Start a full exam"),
+      ("x", "stop_exam", "Stop a full exam"),
    ]
 
    CSS_PATH = "menu.tcss" 
 
    def compose(self):
       yield Header()
-      yield MockExam(id="mockExam")
-      yield MainMenu(id="mainMenu")
+      yield ScrollableContainer(id="screen")
       yield Footer()
+
+   def action_launch_exam(self) -> None:
+      """Called to start a full cfa mock exams """
+      new_exam = MockExam([["""
+This is an example of Textual's `Markdown` widget ?
+
+| Aligné à gauche  | Centré          | Aligné à droite |
+| :--------------- |:---------------:| -----:|
+| Aligné à gauche  |   ce texte        |  Aligné à droite |
+| Aligné à gauche  | est             |   Aligné à droite |
+| Aligné à gauche  | centré          |    Aligné à droite |
+   """, ["Bonjour","Merci","Aurevoir"], 0],["La seconde lettre de l'alphabet ?", ["A","B","C"], 1]])
+      self.query_one("#screen").mount(new_exam)
+
+   def action_stop_exam(self) -> None:
+      """ Called to stop an exam"""
+      exams = self.query("MockExam")
+      if exams:
+         exams.last().remove()
 
 if __name__ == "__main__":
    app = CfaPrepApp()
--- frontend/MockExam.py
+++ frontend/MockExam.py
@@ -3,41 +3,46 @@ from textual.widgets import Button, Label, ProgressBar, Markdown, OptionList, St
 from textual.widgets.option_list import Option, Separator
 from textual.containers import VerticalScroll, Horizontal, Container, Middle, Center
 from TimeDisplay import TimeDisplay
+from textual.reactive import reactive
 
 class MockExam(VerticalScroll):
    """ Custom widget for Questions of a full mock exam """
 
-   markdown_text = """
-# Question
+   curr_question_index = reactive(0)
 
-This is an example of Textual's `Markdown` widget ?
+   def __init__(self, questions):
+      super().__init__()
+      self.questions = questions
 
-| Aligné à gauche  | Centré          | Aligné à droite |
-| :--------------- |:---------------:| -----:|
-| Aligné à gauche  |   ce texte        |  Aligné à droite |
-| Aligné à gauche  | est             |   Aligné à droite |
-| Aligné à gauche  | centré          |    Aligné à droite |
-   """
+   def on_button_pressed(self, event: Button.Pressed) -> None:
+      if event.button.id == "buttonNext":
+         if self.curr_question_index < len(self.questions) - 1:
+            self.curr_question_index += 1
+         else:
+            self.add_class("examEnded")
+
+   def watch_curr_question_index(self, curr_question_index: int) -> None:
+      question, candidates, awnser_index = self.questions[curr_question_index]
+      statementViewer = self.query_one("#statementViewer")
+      statementViewer.update(question)
+
+      options = self.query_one("#options") 
+      options.clear_options()
+      options.add_options(candidates)
 
    def compose(self) -> ComposeResult:
       with Horizontal(id="topBar"):
          with Middle(id="progress"):
             yield Label("Prgress: ")
-            yield ProgressBar(total=180, show_eta=False)
+            yield ProgressBar(total=len(self.questions), show_eta=False)
          with Middle(id="time"):
             yield Label("Time: ")
             yield TimeDisplay()
 
       with Static(id="questionSection"):
          with VerticalScroll(id="question"):
-            yield Markdown(self.markdown_text)
-            yield OptionList(
-               Option("A: Bonjour"),
-               Separator(),
-               Option("B: Aurevoire"),
-               Separator(),
-               Option("C: Merci"),
-            )
+            yield Markdown(id="statementViewer")
+            yield OptionList(id="options")
             with Center():
                yield Button("Next question", variant="success", id="buttonNext")
 
